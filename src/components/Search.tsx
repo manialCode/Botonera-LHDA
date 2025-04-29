a40e5c8ea860964c47742c696d2ee3201983bfc4
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-04-28 21:48:35 -0300

    Agregar componente Search para funcionalidad de búsqueda

---
+++ src/components/Search.tsx
@@ -0,0 +1,60 @@
+interface Props {
+  className: string;
+}
+
+export function Search({ className }: Props) {
+  return (
+    <form className={`flex items-center max-w-sm mx-auto ${className}`}>
+      <label htmlFor="simple-search" className="sr-only">
+        Search
+      </label>
+      <div className="relative w-full">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"
+            />
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="simple-search"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
+          placeholder="Buscar por nombre..."
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="p-2.5 ms-2 text-sm font-medium text-white bg-primary rounded-lg border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary dark:bg-primary dark:hover:bg-yellow-600 dark:focus:ring-primary"
+      >
+        <svg
+          className="w-4 h-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+        <span className="sr-only">Buscar</span>
+      </button>
+    </form>
+  );
+}
