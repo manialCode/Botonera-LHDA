2348c5916e900b4522cdbfec4c91e8425cbd69b9
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-05-02 22:43:27 -0300

    Eliminar la lógica de ordenación en el componente ButtonsPanel y ajustar el estilo del input de búsqueda.

--- src/components/ButtonsPanel.tsx
+++ src/components/ButtonsPanel.tsx
@@ -12,7 +12,6 @@ interface Props {
 
 export default function ButtonsPanel({ videos, error }: Props) {
   const [searchTerm, setSearchTerm] = useState<string | null>(null);
-  const [sortBy, setSortBy] = useState<boolean>(false);
 
   const filterVideos = useMemo(() => {
     console.log("Filtrando videos...");
@@ -29,12 +28,8 @@ export default function ButtonsPanel({ videos, error }: Props) {
     }
 
     // Ordenar por vistas (de mayor a menor)
-    return sortBy ? [...filtered].sort((a, b) => b.views - a.views) : filtered;
-  }, [searchTerm, videos, sortBy]);
-
-  console.log("Término de búsqueda:", searchTerm);
-
-  console.log("Videos filtrados:", filterVideos);
+    return [...filtered].sort((a, b) => b.views - a.views);
+  }, [searchTerm, videos]);
 
   return (
     <>
@@ -45,20 +40,11 @@ export default function ButtonsPanel({ videos, error }: Props) {
         <input
           type="text"
           id="simple-search"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5  dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
           placeholder="Buscar por nombre o link..."
           required
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <div className="flex items-center mt-2 ml-5">
-          <button
-            onClick={() => setSortBy(!sortBy)}
-            type="button"
-            className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          >
-            {sortBy ? "Resetear" : "Popularidad"}
-          </button>
-        </div>
       </div>
 
       {error && <p>Error: {error}</p>}
